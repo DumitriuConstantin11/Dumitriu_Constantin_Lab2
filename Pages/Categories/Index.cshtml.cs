3f21f67591f3ddd9ff7240641da325a7327bdcc1
DumitriuConstantin11 <titirex22@gmail.com>
2024-11-03 21:24:43 +0200

    lab4

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Dumitriu_Constantin_Lab2.Data;
 using Dumitriu_Constantin_Lab2.Models;
+using Dumitriu_Constantin_Lab2.Models.ViewModels;
 
 namespace Dumitriu_Constantin_Lab2.Pages.Categories
 {
@@ -20,10 +21,28 @@ namespace Dumitriu_Constantin_Lab2.Pages.Categories
         }
 
         public IList<Category> Category { get;set; } = default!;
+        public CategoriesIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoriesIndexData();
+            CategoryData.Categories = await _context.Category
+            .Include(i => i.BookCategories)
+            .ThenInclude(bc => bc.Book)
+            .ThenInclude(b => b.Author)
+            .OrderBy(i => i.CategoryName)
+            .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.Books = category.BookCategories.Select(bc => bc.Book).ToList();
+
+            }
         }
     }
 }
